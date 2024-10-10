e26affcc9b751eb1a8a364847181081a9a5e09b7
alexandru233847 <alexutualexino33@gmail.com>
2024-10-10 17:31:33 +0200

    Generate random numbers

---
+++ script.js
@@ -0,0 +1,6 @@
+const numbersElement = document.getElementById('numbers');
+const inputArea = document.getElementById('input-area');
+const submitButton = document.getElementById('submit');
+const gameElement = document.getElementById('result');
+
+// Generate random numbers from 1 to 100
